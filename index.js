c5c26043c5f2c14e4d99778867412b4dfaa20897
phu5554375 <phu5554375@gmail.com>
2021-03-11 09:30:44 +0700

    uppp

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.set("views", "./views");
 app.listen(5000);
 app.use("/uploads", express.static(__dirname + "/../uploads"));
 
-app.use(express.json({limit: '500mb'}));
-app.use(express.urlencoded({limit: '500mb'}));
+app.use(express.json({limit: '800mb'}));
+app.use(express.urlencoded({limit: '800mb'}));
 
 const pg = require("pg");
 //database config
